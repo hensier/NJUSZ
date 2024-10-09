6a0f78aadb0a6dd6504a79b8a6db7ebcbb779f02
hensier <hensier@yeah.net>
2024-10-09 10:13:19 +0800

    Revised IO templates.

--- IO/+IO.cpp
+++ IO/+IO.cpp
@@ -1,3 +1,5 @@
+#include <bits/stdc++.h>
+using namespace std;
 namespace IO {
     char buf[1 << 21], *p1 = buf, *p2 = buf, obuf[1 << 21], *p3 = obuf;
     char gc() {
@@ -29,3 +31,8 @@ namespace IO {
         pc(x % 10 + 48);
     }
 } using namespace IO;
+int main() {
+
+    fwrite(obuf, p3 - obuf, 1, stdout);
+    return 0;
+}
